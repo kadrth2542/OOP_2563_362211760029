5b15f50de635ef33a09a4b07959ec4694f40df0e
kadrth <75652384+kadrth@users.noreply.github.com>
2020-12-15 09:03:39 +0700

    Update HelloWorld.java

--- src/LAB1/HelloWorld.java
+++ src/LAB1/HelloWorld.java
@@ -5,15 +5,18 @@ public class HelloWorld {
     public static void main(String[] args) {
 
        System.out.println("HelloWorld");
+       System.out.println("HelloWorld");
+       System.out.println("HelloWorld");
+       System.out.println("HelloWorld");
+
+
+
+      
 
     }//main
 
 
-    System.out.println("HelloWorld");
-    System.out.println("HelloWorld");
-    System.out.println("HelloWorld");
-
-
+  
 
 
 }//class
